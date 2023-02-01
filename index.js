2a759bdf009ae27dd96ef3756a984d22bda1990f
magaliMoran <biologatlalpan2016@gmail.com>
2023-02-01 00:19:45 -0600

    integrando formularios a la pagina

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-  
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+//   })
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
